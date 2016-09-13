51797de00ef66d66c369d3d96a5b24c729058ae9
Kim Christensen <kich@mvno.dk>
2016-09-13 22:52:50 +0200

    Initial commit

---
+++ src/FuncNet/AssemblyInfo.fs
@@ -0,0 +1,13 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("FuncNet")>]
+[<assembly: AssemblyProductAttribute("FuncNet")>]
+[<assembly: AssemblyDescriptionAttribute("A functional approach to a .NET network framework")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
+    let [<Literal>] InformationalVersion = "1.0"
